b9135750d5a2e505d7345ab662702248ae385514
genedan <genedan@gmail.com>
2020-07-27 18:49:34 -0500

    add functions for ordinary simple interest, banker's rule, classes for compound interest

--- tmval/growth.py
+++ tmval/growth.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from typing import Callable
 
 
@@ -97,8 +99,8 @@ class SimpleAmt(Amount):
 
 class SimpleAcc(Accumulation):
     """
-        Simple interest scenario, special case of accumulation function where growth function is linear
-        """
+    Simple interest scenario, special case of accumulation function where growth function is linear
+    """
     def __init__(
             self,
             s: float
@@ -113,3 +115,104 @@ class SimpleAcc(Accumulation):
     def acc_func(self, t):
         return 1 + self.interest_rate * t
 
+
+def get_simple_amt(pv=None, fv=None, interest=None, n=None):
+    """
+    Simple amount solver for when one variable is missing - returns a simple amount growth class
+    """
+    args = [pv, fv, interest, n]
+    if args.count(None) > 1:
+        raise Exception("Only one argument can be missing.")
+
+    if pv is None:
+        pv = fv / (1 + n * interest)
+    elif fv is None:
+        fv = pv * (1 + n * interest)
+    elif interest is None:
+        interest = (fv / pv - 1) / n
+    else:
+        pass
+
+    simple_amt = SimpleAmt(k=pv, s=interest)
+
+    return simple_amt
+
+
+def osi(beg_dt: dt.datetime, end_dt: dt.datetime, frac=True):
+    """
+    Calculate the number of days using the ordinary simple interest or 30/360 rule.
+    Set frac=True to return days as a percentage of year.
+    """
+
+    y1 = beg_dt.year
+    y2 = end_dt.year
+
+    m1 = beg_dt.month
+    m2 = end_dt.month
+
+    d1 = beg_dt.day
+    d2 = end_dt.day
+
+    days = 360 * (y2 - y1) + 30 * (m2 - m1) + (d2 - d1)
+
+    if frac:
+        return days / 360
+    else:
+        return days
+
+
+def bankers_rule(beg_dt: dt.datetime, end_dt: dt.datetime, frac=True):
+    """
+    Calculate the number of days using the Banker's rule or actual/360 rule.
+    Set frac=True to return days as a percentage of year
+    """
+
+    delta = end_dt - beg_dt
+
+    days = delta.days
+
+    if frac:
+        return days / 360
+    else:
+        return days
+
+
+class CompoundAmt(Amount):
+    """
+    Compound interest scenario, special case of amount function where growth function is geometric
+    """
+    def __init__(
+            self,
+            k: float,
+            i: float
+    ):
+        self.principal = k
+        self.interest_rate = i
+
+        Amount.__init__(
+            self,
+            f=self.amt_func,
+            k=k
+        )
+
+    def amt_func(self, k, t):
+        return k * ((1 + self.interest_rate) ** t)
+
+
+class CompoundAcc(Accumulation):
+    """
+    Compound interest scenario, special case of accumulation function where growth function is geometric
+    """
+    def __init__(
+            self,
+            i: float
+    ):
+        self.interest_rate = i
+
+        Accumulation.__init__(
+            self,
+            f=self.acc_func
+        )
+
+    def acc_func(self, t):
+        return (1 + self.interest_rate) ** t
