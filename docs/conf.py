39d0ddaaa4e01a3788ef1407797623d254b79cdf
genedan <genedan@gmail.com>
2020-07-30 18:38:06 -0500

    add custom css for table alignment

--- docs/conf.py
+++ docs/conf.py
@@ -57,11 +57,16 @@ html_theme_options = {
     'navigation_depth': 3
 }
 
+
 html_favicon = '_templates/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css'
+]
 
 imgmath_image_format = 'svg'
 
