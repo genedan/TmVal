8569a569f73c9a7e1398d7f21e6e341bbc6c6543
genedan <genedan@gmail.com>
2020-08-04 18:17:04 -0500

    add aliases for different compound types

---
+++ tmval/constants.py
@@ -0,0 +1,22 @@
+FORMAL_PATTERNS = {
+    'interest': 'Effective Interest',
+    'i': 'Effective Interest',
+    'APY': 'Effective Interest',
+    'Effective Interest': 'Effective Interest',
+
+    'd': 'Effective Discount',
+    'Effective Discount': 'Effective Discount',
+
+    'nomint': 'Nominal Interest',
+    'nominal interest': 'Nominal Interest',
+    'APR': 'Nominal Interest',
+    'Nominal Interest': 'Nominal Interest',
+
+    'nomdisc': 'Nominal Discount',
+    'nominal discount': 'Nominal Discount',
+    'Nominal Discount': 'Nominal Discount',
+
+    'delta': 'Force of Interest',
+    'force': 'Force of Interest',
+    'Force of Interest': 'Force of Interest'
+}
