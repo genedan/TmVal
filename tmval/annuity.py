b2237d1b54aafb80ed2c257a1d86f2f30a75d8eb
genedan <genedan@gmail.com>
2020-08-14 17:58:56 -0500

    update styling

--- tmval/annuity.py
+++ tmval/annuity.py
@@ -14,10 +14,12 @@ class Annuity(Payments):
         term,
         gr,
         amount=1.0,
+        imd: str = 'immediate'
     ):
         self.term = term
         self.amount = amount
         self.period = period
+        self.imd = imd
 
         n_payments = term / period
         n_payments = int(ceil(n_payments))
@@ -32,72 +34,166 @@ class Annuity(Payments):
 
     def pv(self):
 
-        pv = self.amount * (1 - self.acc.discount_func(self.term)) / (self.acc.val(self.period) - 1)
+        pv = self.amount * \
+             (1 - self.acc.discount_func(self.term)) /\
+             (self.acc.val(self.period) - 1)
 
         return pv
 
     def sv(self):
 
-        sv = self.amount * ((1 + self.acc.interest_rate) ** self.term - 1) / (self.acc.val(self.period) - 1)
+        sv = self.amount * \
+             ((1 + self.acc.interest_rate) ** self.term - 1) / \
+             (self.acc.val(self.period) - 1)
 
         return sv
 
 
-def get_loan_amt(down_pmt: float, loan_pmt: float, period: float, term: float, gr: Rate) -> float:
+def get_loan_amt(
+        down_pmt: float,
+        loan_pmt: float,
+        period: float,
+        term: float,
+        gr: Rate
+) -> float:
 
-    ann = Annuity(period=period, term=term, amount=loan_pmt, gr=gr)
+    ann = Annuity(
+        period=period,
+        term=term,
+        amount=loan_pmt,
+        gr=gr
+    )
 
     loan_amt = ann.pv() + down_pmt
 
     return loan_amt
 
 
-def get_loan_pmt(loan_amt: float, period: float, term: float, gr: Rate, cents=False):
+def get_loan_pmt(
+        loan_amt: float,
+        period: float,
+        term: float,
+        gr: Rate,
+        cents=False
+):
 
-    ann = Annuity(period=period, term=term, gr=gr)
+    ann = Annuity(
+        period=period,
+        term=term,
+        gr=gr
+    )
 
     pmt = loan_amt / ann.pv()
 
     if cents:
+
         acc = CompoundAcc(gr=gr)
+
         pmt_round = round(pmt, 2)
-        pv = Annuity(amount=pmt_round, period=period, term=term, gr=gr).pv()
+
+        pv = Annuity(
+            amount=pmt_round,
+            period=period,
+            term=term,
+            gr=gr
+        ).pv()
+
         if loan_amt == round(pv, 2):
+
             return pmt
+
         else:
+
             cent = decimal.Decimal('0.01')
-            pmt_round2 = float(decimal.Decimal(pmt).quantize(cent, rounding=decimal.ROUND_UP))
-            diff = Annuity(amount=pmt_round2, period=period, term=term, gr=gr).pv() - loan_amt
+
+            pmt_round2 = float(
+                decimal.Decimal(pmt).quantize(
+                    cent,
+                    rounding=decimal.ROUND_UP
+                )
+            )
+
+            diff = Annuity(
+                amount=pmt_round2,
+                period=period,
+                term=term,
+                gr=gr
+            ).pv() - loan_amt
+
             last_pmt = pmt_round2 - round(diff * acc.val(t=term), 2)
-            Installments = namedtuple('installments', 'amount last')
+
+            Installments = namedtuple(
+                'installments',
+                'amount last'
+            )
+
             return Installments(pmt_round2, last_pmt)
     else:
         return pmt
 
 
-def get_savings_pmt(fv: float, period: float, term: float, gr: Rate, cents=False):
+def get_savings_pmt(
+        fv: float,
+        period: float,
+        term: float,
+        gr: Rate,
+        cents=False
+):
 
-    ann = Annuity(period=period, term=term, gr=gr)
+    ann = Annuity(
+        period=period,
+        term=term,
+        gr=gr
+    )
 
     pmt = fv / ann.sv()
 
     if cents:
         pmt_round = round(pmt, 2)
-        fv2 = Annuity(amount=pmt_round, period=period, term=term, gr=gr).sv()
+
+        fv2 = Annuity(
+            amount=pmt_round,
+            period=period,
+            term=term,
+            gr=gr
+        ).sv()
+
         if fv == round(fv2, 2):
+
             return pmt
+
         else:
             cent = decimal.Decimal('0.01')
-            pmt_round2 = float(decimal.Decimal(pmt).quantize(cent, rounding=decimal.ROUND_UP))
-            diff = Annuity(amount=pmt_round2, period=period, term=term, gr=gr).sv() - fv
+
+            pmt_round2 = float(
+                decimal.Decimal(pmt).quantize(
+                    cent,
+                    rounding=decimal.ROUND_UP
+                )
+            )
+
+            diff = Annuity(
+                amount=pmt_round2,
+                period=period,
+                term=term,
+                gr=gr
+            ).sv() - fv
+
             last_pmt = round(pmt_round2 - round(diff, 2), 2)
+
             Installments = namedtuple('installments', 'amount last')
+
             return Installments(pmt_round2, last_pmt)
     else:
         return pmt
 
 
-def get_number_of_pmts(pmt: float, fv: float, period: float, gr: Rate):
+def get_number_of_pmts(
+        pmt: float,
+        fv: float,
+        period: float,
+        gr: Rate
+):
 
     i = gr.convert_rate(
         'Effective Interest',
