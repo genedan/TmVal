47a5d8b8f3019807bd3cd68c3bb7143495a47515
genedan <genedan@gmail.com>
2020-08-17 18:45:29 -0500

    begin work on generalizing annuity arguments

--- tmval/annuity.py
+++ tmval/annuity.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 import decimal
 import numpy as np
-from typing import Union
+from typing import Callable, Union
 
 from tmval.value import Payments, Rate
 from tmval.growth import Accumulation
@@ -12,10 +12,11 @@ class Annuity(Payments):
 
     def __init__(
         self,
-        period,
-        term,
-        gr,
-        amount=1.0,
+        gr: Union[float, Rate, Accumulation, Callable],
+        period: float = None,
+        term: float = None,
+        amount: Union[float, list] = 1.0,
+        times: list = None,
         imd: str = 'immediate'
     ):
         self.term = term
@@ -23,8 +24,7 @@ class Annuity(Payments):
         self.period = period
         self.imd = imd
         imd_ind = 1 if imd == 'immediate' else 0
-
-        n_payments = term / period
+        self.is_level_pmt = None
 
         if term == np.inf:
 
@@ -33,9 +33,33 @@ class Annuity(Payments):
             self._ann_perp = 'perpetuity'
         else:
 
-            n_payments = int(ceil(n_payments))
-            amounts = [amount] * n_payments
-            times = [period * (x + imd_ind) for x in range(n_payments)]
+            if isinstance(amount, float) or isinstance(amount, list) and len(amount) == 1:
+                n_payments = term / period
+                n_payments = int(ceil(n_payments))
+                amounts = [amount] * n_payments
+                times = [period * (x + imd_ind) for x in range(n_payments)]
+                self.is_level_pmt = True
+            else:
+                amounts = amount
+                times = times
+                times.sort()
+                intervals = list(np.diff(times))
+                intervals = [round(x, 7) for x in intervals]
+                if intervals[1:] != intervals[:-1]:
+                    raise Exception("Non-level intervals detected, use Payments class instead.")
+                else:
+                    self.period = intervals[0]
+
+                if amounts[1:] == amounts[:-1]:
+                    self.is_level_pmt = False
+
+                if min(times) == 0:
+                    self.imd = 'due'
+                    self.term = max(times) + self.period
+                else:
+                    self.imd = 'immediate'
+                    self.term = max(times)
+
             self._ann_perp = 'annuity'
 
         Payments.__init__(
@@ -51,29 +75,42 @@ class Annuity(Payments):
             raise ValueError('imd can either be immediate or due.')
 
     def pv(self):
-        if self._ann_perp == 'perpetuity':
 
-            pv = self.amount / (self.gr.val(self.period) - 1)
+        # if interest rate is level, can use formulas to save time
+        if isinstance(self.gr, Accumulation) and self.gr.is_level and self.is_level_pmt:
 
+            if self._ann_perp == 'perpetuity':
+
+                pv = self.amount / (self.gr.val(self.period) - 1)
+
+            else:
+
+                pv = self.amount * \
+                     (1 - self.gr.discount_func(self.term)) /\
+                     (self.gr.val(self.period) - 1)
+
+                if self.imd == 'due':
+                    pv = pv * self.gr.val(self.period)
+
+        # otherwise, use npv function
         else:
-
-            pv = self.amount * \
-                 (1 - self.gr.discount_func(self.term)) /\
-                 (self.gr.val(self.period) - 1)
-
-        if self.imd == 'due':
-            pv = pv * self.gr.val(self.period)
+            pv = self.npv()
 
         return pv
 
     def sv(self):
 
-        sv = self.amount * \
-             ((1 + self.gr.interest_rate) ** self.term - 1) / \
-             (self.gr.val(self.period) - 1)
+        if isinstance(self.gr, Accumulation) and self.gr.is_level and self.is_level_pmt:
+            sv = self.amount * \
+                 ((1 + self.gr.interest_rate) ** self.term - 1) / \
+                 (self.gr.val(self.period) - 1)
 
-        if self.imd == 'due':
-            sv = sv * self.gr.val(self.period)
+            if self.imd == 'due':
+                sv = sv * self.gr.val(self.period)
+
+        else:
+
+            sv = self.eq_val(t=self.term)
 
         return sv
 
@@ -266,7 +303,8 @@ def olb_p(
     term: float,
     gr: Union[float, Rate, Accumulation],
     t: float,
-    r: float = None
+    r: float = None,
+    missed: list = None
 ) -> float:
     """
     Outstanding loan balance - prospective method.
@@ -283,9 +321,12 @@ def olb_p(
     :type t:
     :param r:
     :type r:
+    :param missed:
+    :type missed:
     :return:
     :rtype:
     """
+    acc = Accumulation(gr=gr)
 
     if r is not None:
         ann = Annuity(
@@ -295,10 +336,9 @@ def olb_p(
             amount=q
         )
 
-        acc = Accumulation(gr=gr)
         r_pv = r * acc.discount_func(term - t)
 
-        return ann.pv() + r_pv
+        olb = ann.pv() + r_pv
 
     else:
         ann = Annuity(
@@ -308,4 +348,11 @@ def olb_p(
             amount=q
         )
 
-        return ann.pv()
+        olb = ann.pv()
+
+    if missed:
+
+        for p in missed:
+            olb += q * acc.val(t - p)
+
+    return olb
