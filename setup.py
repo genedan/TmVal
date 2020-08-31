c854a3445564cf43c2062bc68996a5bb103d2a98
genedan <genedan@gmail.com>
2020-08-30 20:01:25 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tmval",
-    version="0.0.7",
+    version="0.0.8",
     author="Gene Dan",
     author_email="genedan@gmail.com",
     description="Time Value of Money",
