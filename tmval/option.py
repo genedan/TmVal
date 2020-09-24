6f6e765b1c9970c927d68f220a5800793cf93809
genedan <genedan@gmail.com>
2020-09-23 23:31:47 -0500

    continue work on binomial pricing

--- tmval/option.py
+++ tmval/option.py
@@ -50,18 +50,153 @@ class Call:
 
         return (c0 * u - vu * s0) / (c0 - vu / rf_fac)
 
+    def binomial_delta(self, u, d, nu, nd, gr, period):
+
+        return binomial_delta(
+            s0=self.s0,
+            k=self.k,
+            n=self.n,
+            t=self.t,
+            u=u,
+            d=d,
+            nu=nu,
+            nd=nd,
+            gr=gr,
+            period=period,
+            option='call'
+        )
+
+    def binomial_f(self, u, d, nu, nd, gr, period):
+
+        return binomial_f(
+            n=self.n,
+            s0=self.s0,
+            t=self.t,
+            k=self.k,
+            u=u,
+            d=d,
+            nu=nu,
+            nd=nd,
+            gr=gr,
+            period=period,
+            option='call'
+        )
+
+    def binomial_st(self, u, d, nu, nd):
+        return binomial_st(s0=self.s0, n=self.n, u=u, d=d, nu=nu, nd=nd)
+
+    def binomial_node(self, u, d, nu, nd, gr, period):
+
+        return binomial_node(
+            s0=self.s0,
+            n=self.n,
+            t=self.t,
+            k=self.k,
+            u=u,
+            d=d,
+            nu=nu,
+            nd=nd,
+            gr=gr,
+            period=period,
+            option='call'
+        )
+
 
 class Put:
     def __init__(
         self,
         n,
         k,
-        price
+        t,
+        s0=None,
+        c0=None
     ):
 
         self.n = n
         self.k = k
-        self.price = price
+        self.s0 = s0
+        self.t = t
+        self.c0 = c0
 
     def payoff(self, stp, cost):
-        return self.n * max(stp, self.k) - self.price - cost
+        return self.n * max(stp, self.k) - self.s0 - cost
+
+    def binomial_st(self, u, d, nu, nd):
+        return binomial_st(s0=self.s0, n=self.n, u=u, d=d, nu=nu, nd=nd)
+
+    def binomial_delta(self, u, d, nu, nd, gr, period):
+        return binomial_delta(
+            s0=self.s0,
+            k=self.k,
+            n=self.n,
+            t=self.t,
+            u=u,
+            d=d,
+            nu=nu,
+            nd=nd,
+            gr=gr,
+            period=period,
+            option='put'
+        )
+
+
+def binomial_st(s0, n, u, d, nu, nd):
+    return n * s0 * (1 + u) ** nu * (1 - d) ** nd
+
+
+def binomial_node(s0, n, t, k, u, d, nu, nd, gr, period, option):
+
+    n_periods = t / period
+
+    if nu + nd == n_periods:
+
+        st = binomial_st(
+            s0=s0,
+            n=n,
+            u=u,
+            d=d,
+            nu=nu,
+            nd=nd
+        )
+
+        if option == 'call':
+            return max(st - k * n, 0)
+        elif option == 'put':
+            return max(k * st - st, 0)
+        else:
+            raise ValueError("Invalid option type specified")
+    else:
+        delta = binomial_delta(s0=s0, k=k, n=n, t=t, u=u, d=d, nu=nu, nd=nd, gr=gr, period=period, option=option)
+        f = binomial_f(s0=s0, t=t, k=k, n=n, u=u, d=d, nu=nu, nd=nd, gr=gr, period=period, option=option)
+        st = binomial_st(s0=s0, n=n, u=u, d=d, nu=nu, nd=nd)
+    return f + delta * st
+
+
+def binomial_delta(s0, k, n, t, u, d, nu, nd, gr, period, option):
+
+    if nu + nd > t / period - 1:
+        raise ValueError("Steps exceed option length.")
+
+    vu = binomial_node(s0=s0, k=k, n=n, t=t, u=u, d=d, nu=nu+1, nd=nd, gr=gr, period=period, option=option)
+    vd = binomial_node(s0=s0, k=k, n=n, t=t, u=u, d=d, nu=nu, nd=nd+1, gr=gr, period=period, option=option)
+
+    su = binomial_st(s0=s0, n=n, u=u, d=d, nu=nu+1, nd=nd)
+    sd = binomial_st(s0=s0, n=n, u=u, d=d, nu=nu, nd=nd+1)
+
+    return (vu - vd) / (su - sd)
+
+
+def binomial_f(n, s0, t, k, u, d, nu, nd, gr, period, option):
+
+    if nu + nd > t / period - 1:
+        raise ValueError("Steps exceed option length.")
+
+    rf_factor = Accumulation(gr=gr).discount_func(t=period)
+
+    vu = binomial_node(s0=s0, n=n, u=u, d=d, t=t, k=k, nu=nu + 1, nd=nd, gr=gr, period=period, option=option)
+    vd = binomial_node(s0=s0, n=n, u=u, d=d, t=t, k=k, nu=nu, nd=nd + 1, gr=gr, period=period, option=option)
+
+    su = binomial_st(n=n, s0=s0, u=u, d=d, nu=nu + 1, nd=nd)
+    sd = binomial_st(n=n, s0=s0, u=u, d=d, nu=nu, nd=nd + 1)
+
+    return rf_factor * (su * vd - sd * vu) / (su - sd)
