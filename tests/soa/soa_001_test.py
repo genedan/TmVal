03b9914491da3fce323cafc0c35ccd1e923797f0
genedan <genedan@gmail.com>
2021-06-01 21:46:19 -0500

    test answer for SOA 001

---
+++ tests/soa/soa_001_test.py
@@ -0,0 +1,7 @@
+from tmval import Rate
+
+delta = Rate(rate=.04, pattern="Nominal Interest", freq=2).convert_rate(pattern="Force of Interest")
+
+
+def test_result():
+    assert round(delta, 4) == .0396
